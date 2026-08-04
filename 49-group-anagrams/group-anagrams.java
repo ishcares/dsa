3f5acaf006ae64b879da351fbd48aa9e7f300ae9
Ishita Chaurasia <ishita20004@gmail.com>
2026-08-04 15:26:03 +0530

    Time: 19 ms (15.93%) | Memory: 50.9 MB (7.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -7,8 +7,12 @@ class Solution {
        Map<String,List<String>> map = new HashMap<>();
        for(String s : strs){
        char[] chars = s.toCharArray();
-       Arrays.sort(chars);
-       String key = new String(chars);
+       int[] freq = new int[26];
+       for(char c : chars){
+        freq[c-'a']++;
+       }
+       
+       String key = Arrays.toString(freq);
       map.computeIfAbsent(key, k-> new ArrayList<>()).add(s);
        }
         return new ArrayList<>(map.values());
