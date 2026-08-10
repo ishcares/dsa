794cbf9155dcc86f3e1b2d8bf2845924685e2eee
Ishita Chaurasia <ishita20004@gmail.com>
2026-08-10 18:52:39 +0530

    Time: 745 ms (5.08%) | Memory: 49.1 MB (98.71%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,20 +1,51 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        
-        if(strs == null||strs.length == 0)
-        return new ArrayList<>();
+    public List<List<String>> groupAnagrams(String[] srs) {
+      List<List<String>> result = new ArrayList<>();
 
-       Map<String,List<String>> map = new HashMap<>();
-       for(String s : strs){
-       char[] chars = s.toCharArray();
-       char[] freq = new char[26];
-       for(char c : chars){
-        freq[c-'a']++;
-       }
-       
-       String key = new String (freq);
-      map.computeIfAbsent(key, k-> new ArrayList<>()).add(s);
-       }
-        return new ArrayList<>(map.values());
+        boolean[] visited = new boolean[srs.length];
+
+        for(int i =0;i<srs.length;i++){
+            if(visited[i]==true){
+                continue;
+            }
+
+            List<String> currentgroup = new ArrayList<>();
+            currentgroup.add(srs[i]);
+            visited[i]=true;
+            for(int j=i+1;j<srs.length;j++){
+                if(visited[j]==true){
+                continue;
+            }
+            if(isAnagram(srs[i],srs[j])){
+                currentgroup.add(srs[j]);
+                visited[j]= true;
+            }
+            
+
+            }
+        result.add(currentgroup);
+        }
+                    return result;
+
     }
+        private boolean isAnagram(String s, String t) {
+        int[] counts = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        for(int i = 0 ; i<s.length(); i++){
+            counts[s.charAt(i)-'a']++;
+            counts[t.charAt(i)-'a']--;
+        }
+        for(int count:counts){
+         if(count!=0)
+            return false;
+        
+
+        }
+       
+        return true;
+        }
+    
 }
