6c022d00a2215ff7c3f0bb8935792d0ab45e488b
Ishita Chaurasia <ishita20004@gmail.com>
2026-08-11 19:28:30 +0530

    Time: 12 ms (84.71%) | Memory: 47.9 MB (19.98%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,26 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent(int[] nums, int k) 
+    {     List<Integer>[] bucket = new List [nums.length+1];
           HashMap <Integer,Integer> map = new HashMap<>();
           for(int num :nums){
-            if(map.containsKey(num)){
-                int currentcount = map.get(num);
-                map.put(num,currentcount+1);
-            }
-            else
-             map.put(num,1);
+            map.put(num,map.getOrDefault(num,0)+1);
           }
-          List <Integer> unique = new ArrayList(map.keySet());
-          unique.sort((a,b)->map.get(b)-map.get(a));
-
+          for(int key:map.keySet()){
+            int index = map.get(key);
+             if(bucket[index]==null){
+                bucket[index]= new ArrayList<>();
+             }
+             bucket[index].add(key);
+          }
+          int pos = 0;
           int[] result = new int[k];
-          for(int i = 0;i<k;i++){
-            result[i] = unique.get(i);
+          for(int i = bucket.length-1;i>0;i--){
+            if(bucket[i]!=null){
+                for(int j = 0; j<bucket[i].size() && pos<k;j++){
+                    result[pos] = bucket[i].get(j);
+                    pos++;
+                }
+            }
           }
           return result;
 
